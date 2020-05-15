7819d9a154926ca09548d9231d95f4575132f892
pranshu2610 <b218030@iiit-bh.ac.in>
2020-05-15 14:21:31 +0530

    Updates

---
+++ precache-manifest.7bb7f0c9083781e746e52b58e0247e3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4557753bca0a480914c54030c408700",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "5b87fe68e6224f804fc5",
+    "url": "/monster-rolodex/static/css/main.9f99ecfe.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b87fe68e6224f804fc5",
+    "url": "/monster-rolodex/static/js/main.f51c8665.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
